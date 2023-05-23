f60438c4e7633df6e5f4ffd68cfa320105e35b79
Fernandoasf <131702359+Fernandoasf@users.noreply.github.com>
2023-05-23 15:05:20 -0300

    modified:   eng_soft/temperatura.py

--- eng_soft/temperatura.py
+++ eng_soft/temperatura.py
@@ -1,4 +1,13 @@
 def conversao(op1,graus):
+    """
+    >>> conversao(1, 32)
+    89.6
+    >>> conversao(2, 89.6)
+    32
+    >>> conversao(3)
+    "Op invalida. digite 1 para celsiu para fahrenheit ou 2 para Fahrenheint para Celsius"3
+
+    """
     if op1 == 1:
         print("Celsius para Fahrenheit")
         fa = (graus * 9/5) + 32
@@ -6,6 +15,7 @@ def conversao(op1,graus):
     elif op1 == 2:
         print("Fahrenheint para Celsius")
         cel = (graus - 32) * 5/9
+        return cel
     else:
         return "Op invalida. digite 1 para celsiu para fahrenheit ou 2 para Fahrenheint para Celsius"
 
@@ -14,3 +24,5 @@ temp = float(input("Digite a Temperatura a sr convertida: "))
 
 result = conversao(op, temp)
 print("A conversão é: ", result)
+
+
